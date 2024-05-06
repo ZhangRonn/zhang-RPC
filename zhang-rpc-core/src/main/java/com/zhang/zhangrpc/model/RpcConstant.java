79f07e3379a2dd0936a1e79bd2fd06a2688e3f44
“ZhangRonn” <2830913880@qq.com>
2024-05-06 15:30:43 +0800

    a

---
+++ zhang-rpc-core/src/main/java/com/zhang/zhangrpc/model/RpcConstant.java
@@ -0,0 +1,17 @@
+package com.zhang.zhangrpc.model;
+
+/**
+ * RPC 相关常量
+ */
+public interface RpcConstant {
+
+    /**
+     * 默认配置文件加载前缀
+     */
+    String DEFAULT_CONFIG_PREFIX = "rpc";
+
+    /**
+     * 默认服务版本
+     */
+    String DEFAULT_SERVICE_VERSION = "1.0";
+}
